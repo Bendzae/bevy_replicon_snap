f2826d87ced7255780f93a2e3b4e423083183c31
Olle Lukowski <ollelukowski@hotmail.com>
2024-04-22 08:34:29 +0200

    fix dumb mistake

--- src/bevy_types.rs
+++ src/bevy_types.rs
@@ -3,6 +3,12 @@ use crate::Interpolate;
 
 impl Interpolate for Transform {
     fn interpolate(&self, other: Self, t: f32) -> Self {
-        self.lerp(other, t)
+        let mut to_return = self.clone();
+
+        to_return.translation = self.translation.lerp(other.translation, t);
+        to_return.rotation = self.rotation.slerp(other.rotation, t);
+        to_return.scale = self.scale.lerp(other.scale, t);
+
+        to_return
     }
 }
